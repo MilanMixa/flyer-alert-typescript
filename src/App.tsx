e43d78176a6ccb228ad4a8283a7dfde4cc03c6b4
MilanMixa <stankovicmilan990@gmail.com>
2022-08-09 15:40:04 +0200

    finished logic for opening and closing options with radio buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Options from "./components/Options";
-import { data, data1, data2 } from "./data/optionsData";
+import { data, data1, data2, data3 } from "./data/optionsData";
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
       <Options optionsArray={data} title={"Product"} id={0} />
       <Options optionsArray={data1} title={"Format"} id={1} />
       <Options optionsArray={data2} title={"Material"} id={2} />
+      <Options optionsArray={data3} title={"Color"} id={3} />
     </div>
   );
 }
--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -14,57 +14,44 @@ export type Props = {
 
 const Options = ({ optionsArray, title, id }: Props) => {
   const { count, setCount } = useContext(OptionsContext);
-  console.log(count);
   const [selected, setSelected] = useState("");
-  console.log(selected);
-  const [showing, setShowing] = useState(true);
 
   const handleChange = (e: any) => {
     setSelected(e.target.value);
-  };
-
-  const handleClick = () => {
-    setShowing((p) => !p);
-
     if (count <= id) {
       setCount(id + 1);
     }
+  };
+
+  const handleClick = () => {
+    setSelected("");
+
     if (count > id) {
       setCount(id);
     }
   };
 
   return (
-    <div className="option">
-      <div onClick={handleClick}>
-        {!showing ? (
-          <div className="title__picked">
-            <h2 className="option__title">{title}</h2> <p>{selected}</p>
-          </div>
-        ) : (
-          <p>{title}</p>
-        )}
+    <div className="option" onClick={handleClick}>
+      <div className="title__picked">
+        <h2 className="option__title">{title}</h2> <p>{selected}</p>
       </div>
-      {optionsArray.map((option, index) => {
-        return (
-          <div key={index} className="option__choices">
-            {count === id && (
-              <>
-                <input
-                  type="radio"
-                  name="options"
-                  id={option.name}
-                  value={option.name}
-                  //  checked={selected === option.name}
-                  onChange={handleChange}
-                  onClick={handleClick}
-                />
-                <label htmlFor={option.name}>{option.name}</label>
-              </>
-            )}
-          </div>
-        );
-      })}
+
+      {count === id &&
+        optionsArray.map((option, index) => {
+          return (
+            <div key={index} className="option__choices">
+              <input
+                type="radio"
+                name="options"
+                id={option.name}
+                value={option.name}
+                onChange={handleChange}
+              />
+              <label htmlFor={option.name}>{option.name}</label>
+            </div>
+          );
+        })}
     </div>
   );
 };
