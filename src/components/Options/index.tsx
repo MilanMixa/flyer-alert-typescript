e854ec00b6f5094806149f0b6348cbe503dd41cf
MilanMixa <stankovicmilan990@gmail.com>
2022-08-10 00:04:31 +0200

    Added Framer Motion the project and implemented it in the options component

--- src/components/Options/index.tsx
+++ src/components/Options/index.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import { useContext, useState } from "react";
 import "./Options.css";
 import { OptionsContext } from "./OptionsContext";
@@ -10,6 +11,14 @@ export type Props = {
   optionsArray: OptionsType[];
   title: string;
   id: number;
+};
+
+const open = {
+  height: "auto",
+};
+
+const closed = {
+  height: 20,
 };
 
 const Options = ({ optionsArray, title, id }: Props) => {
@@ -32,27 +41,37 @@ const Options = ({ optionsArray, title, id }: Props) => {
   };
 
   return (
-    <div className="option" onClick={handleClick}>
-      <div className="title__picked">
-        <h2 className="option__title">{title}</h2> <p>{selected}</p>
-      </div>
+    <AnimatePresence>
+      <motion.div
+        className="option"
+        onClick={handleClick}
+        //  transition={{ ease: "easeOut", duration: 2 }}
+        animate={count === id ? open : closed}
+        initial={{ height: 20 }}
+        exit={{ height: 20 }}
+        transition={{ duration: 0.2 }}
+      >
+        <div className="title__picked">
+          <h2 className="option__title">{title}</h2> <p>{selected}</p>
+        </div>
 
-      {count === id &&
-        optionsArray.map((option, index) => {
-          return (
-            <div key={index} className="option__choices">
-              <input
-                type="radio"
-                name="options"
-                id={option.name}
-                value={option.name}
-                onChange={handleChange}
-              />
-              <label htmlFor={option.name}>{option.name}</label>
-            </div>
-          );
-        })}
-    </div>
+        {count === id &&
+          optionsArray.map((option, index) => {
+            return (
+              <div key={index} className="option__choices">
+                <input
+                  type="radio"
+                  name="options"
+                  id={option.name}
+                  value={option.name}
+                  onChange={handleChange}
+                />
+                <label htmlFor={option.name}>{option.name}</label>
+              </div>
+            );
+          })}
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
